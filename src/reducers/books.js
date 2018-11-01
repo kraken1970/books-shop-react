11e409f9d80e03c1165b944be7ba9399756a1fcc
Serg Minin <everestdon@ukr.net>
2018-11-01 15:38:09 +0300

    add redux

---
+++ src/reducers/books.js
@@ -0,0 +1,26 @@
+const initialState = {
+  books: [
+    {
+      id: 0,
+      title: "Hello!!!!"
+    }
+  ]
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_BOOKS":
+      return {
+        ...state,
+        books: [...state.books, action.payload]
+      };
+    case "SET_BOOKS":
+      return {
+        ...state,
+        books: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
